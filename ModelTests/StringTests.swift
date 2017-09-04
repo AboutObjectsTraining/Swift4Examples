a97234ae89243e16eba5bbb947d9faf18c9801c1
Jonathan Lehr <jonathan@aboutobjects.com>
2017-09-04 13:28:08 -0400

    Swift 4 string indexes

--- ModelTests/StringTests.swift
+++ ModelTests/StringTests.swift
@@ -5,6 +5,21 @@
 
 import XCTest
 
+extension String {
+    public func leadingIndex(of character: Character) -> String.Index {
+        return index(of: character) ?? startIndex
+    }
+    public func trailingIndex(of character: Character) -> String.Index {
+        return index(of: character) ?? endIndex
+    }
+    public func prefix(upTo character: Character) -> String.SubSequence {
+        return self[..<leadingIndex(of: character)]
+    }
+    public func suffix(from character: Character) -> String.SubSequence {
+        return self[trailingIndex(of: character)..<endIndex]
+    }
+}
+
 class StringTests: XCTestCase {
     override func setUp() { super.setUp(); print() }
     override func tearDown() { print(); super.tearDown() }
@@ -14,10 +29,24 @@ class StringTests: XCTestCase {
         print(s.count)
         print(s.contains("World"))
         
-        print(s.substring(to: s.index(of: " ")!))
-        print(s.substring(with: s.index(of: "W")!..<s.index(of: "!")!))
+        let indexOfSpace = s.index(of: " ") ?? s.endIndex
+        print(s[..<indexOfSpace])
         
-        print(s.suffix(from: s.index(of: "W")!))
+        let indexOfW = s.index(of: "W") ?? s.startIndex
+        let indexOfExclamation = s.index(of: "!") ?? s.endIndex
+        print(s[indexOfW...indexOfExclamation])
+        
+        print(s[..<s.leadingIndex(of: " ")])
+        print(s[s.leadingIndex(of: "W")...s.trailingIndex(of: "!")])
+        
+//        print(s.substring(to: s.index(of: " ")!))
+//        print(s.substring(with: s.index(of: "W")!..<s.index(of: "!")!))
+        
+        print(s.prefix(upTo: indexOfSpace))
+        print(s.suffix(from: indexOfW))
+        
+        print(s.prefix(upTo: " "))
+        print(s.suffix(from: "W"))
     }
     
     func testPlist() {
